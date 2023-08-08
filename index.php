57f00e5ac030ec4178d0a647146cf17901dab06b
somitrammh <somitrakaurav@makemyhouse.co.in>
2023-08-08 15:14:41 +0530

    yellow-cap project

---
+++ index.php
@@ -0,0 +1,221 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>Yellow Cap</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+    <!-- Favicon -->
+      <link rel="icon" type="image/x-icon" href="img/fav.png">
+
+    <!-- Google Web Fonts -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@400;700&family=Roboto:wght@400;700&display=swap" rel="stylesheet">  
+
+    <!-- Icon Font Stylesheet -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.0/css/all.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+
+    <!-- Libraries Stylesheet -->
+    <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Template Stylesheet -->
+    <link href="css/style.css" rel="stylesheet">
+</head>
+
+<body>
+
+
+
+    <!-- Navbar Start -->
+    <nav class="navbar navbar-expand-lg navbar-dark shadow-sm px-5 py-3 py-lg-0" id="banner" style="background: #000">
+        <a href="index.html" class="navbar-brand p-0">
+            <img src="img/logo.png" width="200px">
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+            <div class="navbar-nav ms-auto py-0 pe-4 border-end border-5 border-primary">
+             <!--    <a href="index.html" class="nav-item nav-link active">Home</a>
+                <a href="about.html" class="nav-item nav-link">About</a>
+                <a href="service.html" class="nav-item nav-link">Service</a>
+                <div class="nav-item dropdown">
+                    <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
+                    <div class="dropdown-menu m-0">
+                        <a href="blog.html" class="dropdown-item">Blog Grid</a>
+                        <a href="detail.html" class="dropdown-item">Blog Detail</a>
+                        <a href="team.html" class="dropdown-item">The Team</a>
+                        <a href="testimonial.html" class="dropdown-item">Testimonial</a>
+                    </div>
+                </div> -->
+                <!-- <a href="contact.html" class="nav-item nav-link">Contact</a> -->
+            </div>
+            <div class="d-none d-lg-flex align-items-center ps-4">
+                <i class="fa fa-2x fa-phone text-secondary me-3"></i>
+                <div>
+                    <h6 class="text-light m-0">888 888 8888</h6>
+                </div>
+            </div>
+        </div>
+    </nav>
+    <!-- Navbar End -->
+
+
+    <!-- Hero Start -->
+    <div class="container-fluid py-5 vdo" style="overflow: hidden; position: relative;">
+        <div class="container-fluid py-5 ps-5 pe-5 pb-0">
+            <div class="overflow-banner">
+                      <video autoplay muted loop playsinline preload="metadata">
+                          <source src="img/banner.mp4" type="video/mp4" width="100%">
+                      </video>
+                </div>
+            <div class="row justify-content-start">
+                <div class="col-lg-8 text-center text-lg-start">
+                    <h2 class="display-3 text-light">Skilled & Reliable <br><font color="#fdd839" >Contractors</font> at your fingertips! </h2>
+                    <div class="pt-3">
+                       <img src="img/stor.png" class="star-img" width="40%">
+                    </div>
+                </div>
+                <div class="col-lg-4 mbl-banner-img">
+                    <img src="img/mbl-14.png" width="90%">
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Hero End -->
+
+
+    <!-- About Start -->
+    <div class="container-fluid mb-5 pt-2" style="background: #fffbe8">
+         <div class="row gx-0 mb-3 mb-lg-0 pt-5">
+                <div class="col-sm-10 m-auto mb-4" >
+                    <p class="fs-4 text-dark mb-5 text-center" style="font-size: 18px!important;"><font style="font-weight: bold; font-size: 30px;" >Yellow Cap</font> is a network that connects our clients with experienced and professional civil contractors. It has a wide range of professional workers including electricians, plumbers, flooring contractor, and many more, who can conveniently select an order placed by a client. We understand the stress of finding a reliable contractor in a house building process. So, here we are with this hassle-free process of hiring contractors at the ease of a few clicks. </p>
+                </div>
+            </div>
+        </div>
+        <div class="container ex-m-t">
+            <div class="row gx-0 mb-3 mb-lg-0">
+                <div class="col-lg-6 my-lg-5 py-lg-5 mbl-sec-1 ord-2">
+                    <div class="p-5 pt-0 side-app pe-0">
+                        <h1 class="display-5 mb-4 web-ttl">Kya aap Contractors dekh rahein hain? </h1>
+                        <p><img src="img/li.png" width="20px;">&nbsp; 1 Lakh+ registered Civil Engineer & Contractor ki Profile dekhe aur connect kare</p>
+                        <p><img src="img/li.png" width="20px;">&nbsp; Easy chat system se One-to-One Communication kar sakte h</p>
+                        <p><img src="img/li.png" width="20px;">&nbsp; Aasani se apne Order ko create kare aur List kare</p>
+                        <p><img src="img/li.png" width="20px;">&nbsp; Applicants se baat karke contractor final kar sakte h</p>
+                        <img src="img/stor.png" class="mt-3 star-img" width="70%">
+                    </div>
+                </div>
+                <div class="col-lg-6 mbl-sec-2 ord-1" style="min-height: 400px;">
+                    <div class="position-relative h-100 text-center">
+                        <div class="mbl-ttl" >
+                           <h1 class="display-5 mb-4">Kya aap Contractors dekh rahein he? </h1>
+                        </div>
+                        <div class="img-gif" >
+                          <img class="gif-main" src="img/1.gif" width="100%">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- About End -->
+        <!-- About Start -->
+    <div class="container-fluid ex-m-t">
+        <div class="container">
+            <div class="row gx-0 mb-3 mb-lg-0">
+                <div class="col-lg-6 mbl-0 ord-1" style="min-height: 400px;">
+                    <div class="position-relative h-100 text-start">
+                        <div>
+                            <div class="mbl-ttl" >
+                                <h1 class="display-5 mb-4">Kya aap Contractor hain ? Construction ke High-value Projects khoj rahein hain?</h1>
+                           </div>
+                           <div class="img-gif">
+                            <img class="gif-main gif-main-2" src="img/2.gif" width="100%" >
+                           </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 my-lg-5 py-lg-5 ord-2">
+                    <div class="p-5 pt-0 side-app pe-0">
+                        <h1 class="display-5 mb-4 web-ttl">Kya aap Contractor hain? <br> Construction ke High-value Projects khoj rahein hain?</h1>
+                        <p><img src="img/li.png" width="20px;"> &nbsp; Unlimited Civil Projects se FREE me judein</p>
+                        <p><img src="img/li.png" width="20px;"> &nbsp; Payein Lakho ke Contracting  and Sub-Contracting Orders</p>
+                        <p><img src="img/li.png" width="20px;"> &nbsp; Construction se related sabhi tarah ke projects available he </p>
+                        <p><img src="img/li.png" width="20px;"> &nbsp; Engineers and Contractor ki community se judein aur ideas share kare</p>
+                        <img src="img/stor.png" class="mt-3 star-img" width="70%">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- About End -->
+
+    <!-- Footer Start -->
+    <div class="container-fluid bg-dark text-light py-5 mt-5 footer">
+        <div class="container py-5">
+            <div class="row g-5">
+                <div class="col-lg-4 col-md-6">
+                    <h4 class="text-primary">About Us</h4>
+                    <hr class="w-25 text-secondary mb-4" style="opacity: 1;">
+                    <p class="mb-4">Yellow Cap is a network that connects our clients with experienced and professional civil contractors.</p>
+                    <h6 class="text-primary mt-4 mb-3">Contact <I>info</I></h6>
+                    <div class="d-flex mb-4">
+                        <a class="btn btn-lg btn-primary btn-lg-square rounded-circle me-2" href="#"><i class="fab fa-twitter"></i></a>
+                        <a class="btn btn-lg btn-primary btn-lg-square rounded-circle me-2" href="#"><i class="fab fa-facebook-f"></i></a>
+                        <a class="btn btn-lg btn-primary btn-lg-square rounded-circle me-2" href="#"><i class="fab fa-linkedin-in"></i></a>
+                        <a class="btn btn-lg btn-primary btn-lg-square rounded-circle" href="#"><i class="fab fa-instagram"></i></a>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 offset-1">
+                    <h4 class="text-primary">Quick Links</h4>
+                    <hr class="w-25 text-secondary mb-4" style="opacity: 1;">
+                    <div class="d-flex flex-column justify-content-start">
+                        <a class="text-light mb-2" href="#"><i class="fa fa-angle-right me-2"></i>Home</a>
+                        <a class="text-light mb-2" href="#"><i class="fa fa-angle-right me-2"></i>About Us</a>
+                        <a class="text-light mb-2" href="#"><i class="fa fa-angle-right me-2"></i>Our Services</a>
+                        <a class="text-light mb-2" href="#"><i class="fa fa-angle-right me-2"></i>Meet The Team</a>
+                        <a class="text-light mb-2" href="#"><i class="fa fa-angle-right me-2"></i>Latest Blog</a>
+                        <a class="text-light" href="#"><i class="fa fa-angle-right me-2"></i>Contact Us</a>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6">
+                    <h4 class="text-primary">Contact Info</h4>
+                    <hr class="w-25 text-secondary mb-4" style="opacity: 1;">
+                                        <p class="mb-2"><i class="fa fa-map-marker-alt text-primary me-3"></i>123 Street, New York, USA</p>
+                    <p class="mb-2"><i class="fa fa-envelope text-primary me-3"></i>info@example.com</p>
+                    <p class="mb-0"><i class="fa fa-phone-alt text-primary me-3"></i>+012 345 67890</p>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container-fluid bg-primary text-dark py-4">
+        <div class="container">
+            <div class="row g-0">
+                <div class="col-md-12 text-center">
+                    <p class="mb-md-0">Copyright &copy; <a class="text-dark fw-bold" href="#">Yellow Cap</a>. All Rights Reserved.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Footer End -->
+
+
+    <!-- Back to Top -->
+    <a href="#banner" class="btn btn-lg btn-secondary btn-lg-square rounded-circle back-to-top"><i class="bi bi-arrow-up"></i></a>
+
+
+    <!-- JavaScript Libraries -->
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Template Javascript -->
+    <script src="js/main.js"></script>
+</body>
+
+</html>
